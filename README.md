a49f9b07e93e6d6dc7917f1bde5ef085badd2b38
Nayeon Kim <nykim92@gmail.com>
2017-02-11 21:01:40 -0800

    Initial set up

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Moo 2.0
+
+### Local Development
+To start developing, simply clone this repository and install packages using the following commands:
+
+npm install gulp
+
+npm install gulp-sass
+
+npm install browsersync
---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', ['sass'], function() {
+	browserSync.init({
+		server: {
+			baseDir: "source"
+		}
+	});
+});
+
+gulp.task('sass', function() {
+	return gulp.src('source/scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('source/css'))
+		.pipe(browserSync.reload({stream: true})); // prompts a reload after compilation
+});
+
+gulp.task('serve', ['browser-sync'], function() {
+	gulp.watch('source/scss/*.scss', ['sass']);
+	gulp.watch('source/*.html').on('change', browserSync.reload);
+	gulp.watch('source/js/*.js').on('change', browserSync.reload);
+});
