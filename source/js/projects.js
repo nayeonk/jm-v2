228cbdfd9be470560811f4365ae31c25c1e9bd1f
Nayeon Kim <nykim92@gmail.com>
2017-04-21 22:15:49 -0700

    First pass on Projects page

---
+++ source/js/projects.js
@@ -0,0 +1,20 @@
+var recentProjects = [
+	{
+		icon: 'icon_slate_film.png',
+		name: 'Little Bitches',
+		description: 'A group of friends vow to open their college acceptance letters at the same time during the big end-of-year party.',
+		date: 'November 2017'
+	},
+	{
+		icon: 'icon_karaoke.png',
+		name: 'Don\'t Mind Me Ep.3',
+		description: 'David Black is in the studio! He is a professor at the University of Southern California studying mindfulness.',
+		date: 'June 15, 2017'
+	},
+	{
+		icon: 'icon_movie.png',
+		name: 'Behind the Scene',
+		description: 'A group of friends vow to open their college acceptance letters at the same time during the big end-of-year party.',
+		date: 'November 2017'
+	}
+];
