8cd53f0a9e28f484f2054eae17f1eacbec0d854b
Nayeon Kim <nykim92@gmail.com>
2017-05-19 00:13:41 -0700

    Add real projects list

--- source/js/projects.js
+++ source/js/projects.js
@@ -1,32 +1,20 @@
 var recentProjects = [
 	{
-		icon: 'icon_slate_film.png',
+		icon: 'icon_movie.png',
 		name: 'Little Bitches',
 		description: 'A group of friends vow to open their college acceptance letters at the same time during the big end-of-year party.',
-		date: 'November 2017'
+		date: 'November 18, 2017'
 	},
 	{
-		icon: 'icon_mic.png',
+		icon: 'icon_video.png',
+		name: 'Wine and Cheese',
+		description: 'Wine and Cheese is a short film I wrote and directed, coming to Vimeo.',
+		date: 'Summer 2017'
+	},
+	{
+		icon: 'icon_video.png',
 		name: 'Don\'t Mind Me Ep.3',
-		description: 'David Black is in the studio! He is a professor at the University of Southern California studying mindfulness.',
-		date: 'June 15, 2017'
-	},
-	{
-		icon: 'icon_mic.png',
-		name: 'Don\'t Mind Me Ep.3',
-		description: 'David Black is in the studio! He is a professor at the University of Southern California studying mindfulness.',
-		date: 'June 15, 2017'
-	},
-	{
-		icon: 'icon_mic.png',
-		name: 'Don\'t Mind Me Ep.3',
-		description: 'David Black is in the studio! He is a professor at the University of Southern California studying mindfulness.',
-		date: 'June 15, 2017'
-	},
-	{
-		icon: 'icon_movie.png',
-		name: 'Behind the Scene',
-		description: 'A group of friends vow to open their college acceptance letters at the same time during the big end-of-year party.',
-		date: 'November 2017'
+		description: 'A semi-autobiographical pilot I wrote and directed, based on my childhood. It’s an ensemble dramedy about a dysfunctional Mormon family defined by the mom’s cancer and the daughter’s child acting. It’s a private project, currently on Vimeo and viewable to those who either have the password or guess it (don’t get any ideas).',
+		date: 'TBD'
 	}
 ];
