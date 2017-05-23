d131df5e5506a718fd207a2c284b75367fdc7807
Nayeon Kim <nykim92@gmail.com>
2017-05-23 02:23:13 -0700

    Minor edits

--- source/js/projects.js
+++ source/js/projects.js
@@ -2,18 +2,21 @@ var recentProjects = [
 	{
 		icon: 'icon_slate_film.png',
 		name: 'Little Bitches',
+		link: false,
 		description: 'A group of friends vow to open their college acceptance letters at the same time during the big end-of-year party.',
 		date: 'November 18, 2017'
 	},
 	{
 		icon: 'icon_movie.png',
 		name: 'Wine and Cheese',
+		link: false,
 		description: 'Wine and Cheese is a short film I wrote and directed, coming to Vimeo.',
 		date: 'Summer 2017'
 	},
 	{
 		icon: 'icon_movie.png',
-		name: 'Don\'t Mind Me Ep.3',
+		name: 'The McCurdy\'s',
+		link: 'https://vimeo.com/217590566',
 		description: 'A semi-autobiographical pilot I wrote and directed, based on my childhood. It’s an ensemble dramedy about a dysfunctional Mormon family defined by the mom’s cancer and the daughter’s child acting. It’s a private project, currently on Vimeo and viewable to those who either have the password or guess it (don’t get any ideas).',
 		date: 'TBD'
 	}
